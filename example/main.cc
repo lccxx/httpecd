1a2ff5b7175e0a83ac50006d6c274421d3f4617b
Lich <liich@protonmail.com>
2020-03-15 18:25:22 +0800

    Add thread & total count

--- example/main.cc
+++ example/main.cc
@@ -6,16 +6,34 @@
 #include "httpecd/client.h"
 
 int main(int argc, char **args) {
-  constexpr int EPOLL_SIZE = 1024;
+  std::array<uint64_t, 1> sums = {0};      // thread number
+  constexpr uint64_t MAX_REQUEST = 10000;  // per thread request number
 
-  const char host[] = "127.0.0.1";
-  const int port = 80;
-  const std::string req = "GET / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n";
+  for (auto &sum : sums) {
+    std::thread([&sum, &MAX_REQUEST] {
+      constexpr int EPOLL_SIZE = 1024;
 
-  std::vector<int> epolls(EPOLL_SIZE);
-  std::vector<struct epoll_event> events(EPOLL_SIZE);
+      const char host[] = "127.0.0.1";
+      const int port = 80;
+      const std::string req = "GET / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n";
 
-  httpecd::client::batch_request(host, port, req, epolls, events);
+      std::vector<int> epolls(EPOLL_SIZE);
+      std::vector<struct epoll_event> events(EPOLL_SIZE);
+
+      for (sum = 0; sum < MAX_REQUEST; sum += EPOLL_SIZE) {
+        httpecd::client::batch_request(host, port, req, epolls, events);
+      }
+    }).detach();
+  }
+
+  for (int64_t old_sum = 0;;) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    uint64_t total = 0;
+    for (const auto sum : sums) total += sum;
+    printf("per_second: %ld, total: %ld\n", (total - old_sum), total);
+    if (total - old_sum == 0) break;
+    old_sum = total;
+  }
 
   return 0;
 }
